b5440c3c39c5342be91326a8a4414cca7e013890
hwpanda <hwpanda225@gmail.com>
2021-10-26 11:48:07 -0700

    added gitignore and package.json

---
+++ app/main/index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+// const path = require('path');
+// const handleIPC = require('./ipc');
+// const {create: createWindow, show:showMainWindow, close:closeMainWindow} = require('./windows/main');
+// const {create: createControlWindow} = require('./windows/control');
+// const gotTheLock = app.requestSingleInstanceLock();
+
+let win;
+app.on('ready', () => {
+	win = new BrowserWindow({
+		width: 600,
+		height: 300,
+		webPreferences: {
+			nodeIntegration: true,
+			//below 2 lines solve the problem of React not working in Electron
+			enableRemoteModule: true,
+			contextIsolation: false,
+		},
+	});
+	if (isDev) {
+		// load the react html file into the window
+		win.loadURL('http://localhost:3000');
+	} else {
+		win.loadFile(
+			path.resolve(__dirname, '../../renderer/pages/main/index.html')
+		);
+	}
+
+	// win.on('closed', ()=>{
+	//     win = null;
+	// });
+});
