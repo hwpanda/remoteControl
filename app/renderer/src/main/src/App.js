4cd9ce99bccaeeccd7f8ff01a5dbc39b8d34cb72
hwpanda <hwpanda225@gmail.com>
2021-10-26 12:55:57 -0700

    basic logic for app.js

--- app/renderer/src/main/src/App.js
+++ app/renderer/src/main/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 //import { ipcRenderer } from 'electron';
 const { ipcRenderer } = window.require('electron');
 
 function App() {
+	const [remoteCode, setRemoteCode] = useState('');
+	const [localCode, setLocalCode] = useState('');
+	// 0: not connected, 1: controlling, 2: being controlled
+	const [controlText, setControlText] = useState('');
+	// login
+	const login = async () => {
+		let code = await ipcRenderer.invoke('login');
+		setLocalCode(code);
+	};
+	//startControl
+	const startControl = (remoteCode) => {
+		// send request that we want to control the client with remoteCode
+		ipcRenderer.send('control', remoteCode);
+	};
+
+	// handleControlState
+	const handleControlState = (e, name, type) => {
+		let text = '';
+		if (type === '1') {
+			text = `remotely controlling ${name}`;
+		} else if (type === '2') {
+			text = `being controlled by ${name}`;
+		} else {
+			text = '';
+		}
+		setControlText(text);
+	};
+
+	useEffect(() => {
+		login();
+		ipcRenderer.on('control-state-change', handleControlState);
+		//remove event listener on unmount
+		return () => {
+			ipcRenderer.removeListener('control-state-change', handleControlState);
+		};
+	}, []);
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+			{controlText === '' ? (
+				<>
+					<div>Your Local Code {localCode}</div>
+					<input
+						type='text'
+						value={remoteCode}
+						onChange={(e) => setRemoteCode(e.target.value)}
+					/>
+					<button
+						onClick={() => {
+							startControl(remoteCode);
+						}}
+					>
+						confirm
+					</button>
+				</>
+			) : (
+				<div>{controlText}</div>
+			)}
 		</div>
 	);
 }
